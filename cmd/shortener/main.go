7fa41f24b1e72b56626f236d65ebe31a6f488802
Almat Akhat <fabiannsailor@gmail.com>
2022-01-20 00:37:11 +0600

    Increment 7. added flags support.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Fe4p3b/url-shortener/internal/app/shortener"
 	"github.com/Fe4p3b/url-shortener/internal/handlers"
@@ -17,10 +19,25 @@ type Config struct {
 	FileStoragePath string `env:"FILE_STORAGE_PATH,required" envDefault:"/tmp/url_shortener_storage"`
 }
 
-func main() {
-	var cfg Config
-	err := env.Parse(&cfg)
+func setConfig(cfg *Config) error {
+	if len(os.Args) > 1 {
+		flag.StringVar(&cfg.Address, "a", "localhost:8080", "Адрес запуска HTTP-сервера")
+		flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
+		flag.StringVar(&cfg.FileStoragePath, "f", "/tmp/url_shortener_storage", "Путь до файла с сокращёнными URL")
+		flag.Parse()
+		return nil
+	}
+
+	err := env.Parse(cfg)
 	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	cfg := &Config{}
+	if err := setConfig(cfg); err != nil {
 		log.Fatal(err)
 	}
 
